ee5886ab81e7019c9f7940402846a48e7e32ebff
Meir <meirroc5764@gmail.com>
2026-06-22 12:46:35 +0300

    12:45

---
+++ week11/projectDay2/projectDay2c#.cs
@@ -0,0 +1,29 @@
+using System;
+namespace day2
+{
+    class Test
+    {
+        static int userIntInput;
+        static string userStrInput;
+
+
+
+        static List<int> trackId = new List<int>
+        static List<int> trackSpeed = new List<int>
+        static List<string> trackHeading = new list<string>
+
+        static void Main()
+        {
+            bool test = false;
+
+            while (test == false) ;
+            {
+                userIntInput =
+            }
+
+
+
+        }
+    }
+
+}
